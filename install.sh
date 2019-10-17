2302bbd28186fc76bcb43508d58683d5a0ea9d36
Radiantly <44368997+radiantly@users.noreply.github.com>
2019-10-17 09:55:25 +0530

    My favorite font

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt update
-sudo apt install zsh curl
+sudo apt install zsh curl fonts-firacode
 
 # Install oh-my-zsh
+RUNZSH=no
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
