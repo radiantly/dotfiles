5f5c89a78bc37900ae66cd9141a7bf3c408e583a
Radiantly <44368997+radiantly@users.noreply.github.com>
2019-10-19 22:34:19 +0530

    More stuff! Yay! *cringe* :D

---
+++ .vimrc
@@ -0,0 +1,21 @@
+" This automatically installs the vim-plug plugin manager
+" Snippet was taken from the vim-plug wiki
+if empty(glob('~/.vim/autoload/plug.vim'))
+  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
+    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+
+call plug#begin('~/.vim/plugged')
+
+" Declare the list of plugins.
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+
+" List ends here. Plugins become visible to Vim after this call.
+call plug#end()
+
+syntax on
+set number
+
+let g:airline_theme='powerlineish'
--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
+# Let's update package lists!
 sudo apt update
-sudo apt install zsh curl fonts-firacode
+
+packages=(zsh curl fonts-firacode git)
+for package in $packages; do
+	sudo apt -y install $package
+done
 
 # Install oh-my-zsh
-RUNZSH=no
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+
+zsh -c '
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+'
