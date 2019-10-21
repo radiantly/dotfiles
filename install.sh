47a1b08b6950dd97ede549fac541add2a6b84387
Radiantly <44368997+radiantly@users.noreply.github.com>
2019-10-21 16:52:36 +0530

    Added symlinking .zshrc and .vimrc

--- install.sh
+++ install.sh
@@ -9,8 +9,13 @@ done
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
+rm ~/.zshrc ~/.vimrc
+ln -s ./zshrc ~/.zshrc
+ln -s ./vimrc ~/.vimrc
+
 zsh -c '
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 '
+
