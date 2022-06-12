336c9e14f222709830ebd9de8273a8a26b76bdc7
radiantly <buildingsomethingfun@gmail.com>
2022-06-12 22:24:17 +0530

    Switch to pamixer for pipewire support

--- .config/polybar/mic.sh
+++ .config/polybar/mic.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
 status() {
-  MUTED=$(pacmd list-sources | awk '/\*/,EOF {print}' | awk '/muted/ {print $2; exit}')
-
-  if [ "$MUTED" = "yes" ]; then
-    echo " Mute"
-  else
-    echo -n " "
-    pacmd list-sources | grep "\* index:" -A 7 | grep volume | awk -F/ '{print $2}' | tr -d ' '
-  fi
+  echo " $(pamixer --default-source --get-volume-human)"
 }
 
 listen() {
@@ -21,36 +14,15 @@ listen() {
     done
 }
 
-toggle() {
-  MUTED=$(pacmd list-sources | awk '/\*/,EOF {print}' | awk '/muted/ {print $2; exit}')
-  DEFAULT_SOURCE=$(pacmd list-sources | awk '/\*/,EOF {print $3; exit}')
-
-  if [ "$MUTED" = "yes" ]; then
-      pactl set-source-mute "$DEFAULT_SOURCE" 0
-  else
-      pactl set-source-mute "$DEFAULT_SOURCE" 1
-  fi
-}
-
-increase() {
-  DEFAULT_SOURCE=$(pacmd list-sources | awk '/\*/,EOF {print $3; exit}')
-  pactl set-source-volume "$DEFAULT_SOURCE" +5%
-}
-
-decrease() {
-  DEFAULT_SOURCE=$(pacmd list-sources | awk '/\*/,EOF {print $3; exit}')
-  pactl set-source-volume "$DEFAULT_SOURCE" -5%
-}
-
 case "$1" in
     --toggle)
-        toggle
+        pamixer --default-source --toggle-mute
         ;;
     --increase)
-        increase
+        pamixer --default-source --increase 10
         ;;
     --decrease)
-        decrease
+        pamixer --default-source --decrease 10
         ;;
     *)
         listen
